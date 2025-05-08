36b52e3e8d75a361ad9f9385288360b97353035a
Yannick Schmid <yannickschmid.compsci@gmail.com>
2025-05-08 17:31:11 +0200

    logging at test time

--- src/test/java/com/github/schmidya/stomp/client/StompClientTest.java
+++ src/test/java/com/github/schmidya/stomp/client/StompClientTest.java
@@ -4,39 +4,41 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.activemq.ArtemisContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
-import com.github.schmidya.stomp.client.StompClient;
 import com.github.schmidya.stomp.client.frames.StompConnectFrame;
 import com.github.schmidya.stomp.client.frames.StompConnectedFrame;
 import com.github.schmidya.stomp.client.frames.StompServerFrame;
 
 @Testcontainers
 public class StompClientTest {
+        Logger logger = LoggerFactory.getLogger(StompClientTest.class);
 
-    StompClient underTest;
+        StompClient underTest;
 
-    @Container
-    public GenericContainer artemis = new GenericContainer(
-            DockerImageName.parse("docker.io/apache/activemq-artemis:latest-alpine"))
-            .withExposedPorts(61613);
+        @Container
+        public GenericContainer artemis = new ArtemisContainer(
+                        DockerImageName.parse("docker.io/apache/activemq-artemis:latest-alpine")
+                                        .asCompatibleSubstituteFor("apache/activemq-artemis"))
+                        .withExposedPorts(61613);
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        String address = artemis.getHost();
-        Integer port = artemis.getFirstMappedPort();
-        System.out.print("Hello");
-        String url = "tcp://" + address + ":" + port;
-        underTest = StompClient.fromUrl(url);
-    }
+        @BeforeEach
+        public void setUp() throws Exception {
+                String address = artemis.getHost();
+                Integer port = artemis.getFirstMappedPort();
+                String url = "tcp://" + address + ":" + port;
+                underTest = StompClient.fromUrl(url);
+        }
 
-    @Test
-    public void connectTest() throws Exception {
-        StompServerFrame frame = underTest.connect(new StompConnectFrame("artemis", "artemis", "artemis"));
-        assertInstanceOf(StompConnectedFrame.class, frame);
-    }
+        @Test
+        public void connectTest() throws Exception {
+                StompServerFrame frame = underTest.connect(new StompConnectFrame("artemis", "artemis", "artemis"));
+                assertInstanceOf(StompConnectedFrame.class, frame);
+        }
 
 }
